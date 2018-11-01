a3ea8a3f6fc511299647943d96b5e0427d0daeb4
peterwathithi <peterwathithi8@gmail.com>
2018-11-01 16:03:19 +0300

    this is a commit for what i originally planned to be amended

---
+++ htdocs/test.php
@@ -0,0 +1,20 @@
+<?php
+const BR=<br/>
+$Stick_one="mosiamo";
+$STICK_TWO="pixie";
+$stick_three="stugie";
+echo$Stick_on,BR;
+echo$STICK_TWO.BR;
+echo$stick_three.BR;
+
+//if conditions
+$Jina=21;
+$jina=$Jina<13="you can enter"."you cant enter";
+echo $jina;
+
+?>/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 01/10/2018
+ * Time: 18:20
+ */
